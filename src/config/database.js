1b403df4c52dcd260bd1eb4bd6639df7f64d404f
LandryLuamba <146769561+LandryLuamba@users.noreply.github.com>
2026-02-28 00:19:01 -0700

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -10,16 +10,19 @@ const client = new MongoClient(URI, {
     appName: "max-method",
 });
 
+let db;
+
 try {
     // Connect the client to the server
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    db = client.db("Maxmethod_db")
 } catch (err) {
     console.error(err);
 }
 
-let db = client.db("employees");
 
 export default db;
