5b98bec63881aec554b832e93864b6bf743df04e
tuschuk <tristen.uschuk21@gmail.com>
2026-03-10 10:45:53 -0600

    fix API route mount path

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ import users from "./src/routes/userRoutes.js"
 import * as path from "path";
 import dotenv from "dotenv"
 
-
 dotenv.config()
 
 const PORT = process.env.PORT || 5050;
@@ -15,45 +14,39 @@ const mongoURI = process.env.MONGODB_URI;
 const corsOptions = {
     origin: ["http://localhost:5173"],
     optionsSuccessStatus: 200
-
 };
 
 const __dirname = path.resolve();
-
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use("/users", users)
-
+app.use("/api/users", users)
 
 app.set("view-engine","react-html-parser")
+
 // Home, Classification, Goals pages, History, Settings
-app.get("/home", cors(corsOptions), (req, res,next) => {
+app.get("/home", cors(corsOptions), (req, res, next) => {
     res.sendFile(path.join(__dirname, "/src/index.html"));
 });
 
-app.get("/classification", cors(corsOptions), (req, res,next) => {
-    res.sendFile(path.join(__dirname, "/src/index.html"));
-    //next();
-});
-
-app.get("/goals", cors(corsOptions), (req, res,next) => {
-    res.sendFile(path.join(__dirname, "/src/index.html"));
-    //next();
-});
-
-app.get("/history", cors(corsOptions), (req, res,next) => {
+app.get("/classification", cors(corsOptions), (req, res, next) => {
     res.sendFile(path.join(__dirname, "/src/index.html"));
 });
-app.get("/settings", cors(corsOptions), (req, res,next) => {
+
+app.get("/goals", cors(corsOptions), (req, res, next) => {
     res.sendFile(path.join(__dirname, "/src/index.html"));
-})
+});
 
+app.get("/history", cors(corsOptions), (req, res, next) => {
+    res.sendFile(path.join(__dirname, "/src/index.html"));
+});
 
-
+app.get("/settings", cors(corsOptions), (req, res, next) => {
+    res.sendFile(path.join(__dirname, "/src/index.html"));
+});
 
 // start the Express server
 app.listen(PORT, () => {
